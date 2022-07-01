f08a46ac169db1772d03c09e744f64e622a6fcd0
zangchanglong <zangchanglong@camel4u.com>
2022-07-01 14:50:03 +0800

    init commit...

---
+++ python/mtoc/core/_maya.py
@@ -0,0 +1,44 @@
+#========================================
+#    author: Changlong.Zang
+#      mail: zclongpop123@163.com
+#      time: Fri Jul  1 14:18:26 2022
+#========================================
+import maya.cmds as mc
+import pymel.core as pm
+import maya.OpenMaya as OpenMaya
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+def get_ai_tex_data(shader):
+    '''
+    '''
+    data = dict()
+    for attr in ('baseColor', 'specularRoughness', 'opacity', 'normalCamera'):
+        attr_pml_node = pm.PyNode('{0}.{1}'.format(shader, attr))
+        attr_api_node = attr_pml_node.__apiobject__()
+        iterator = OpenMaya.MItDependencyGraph(attr_api_node, 
+                                               OpenMaya.MFn.kFileTexture, 
+                                               OpenMaya.MItDependencyGraph.kUpstream,
+                                               OpenMaya.MItDependencyGraph.kDepthFirst,
+                                               OpenMaya.MItDependencyGraph.kPlugLevel)
+
+        while not iterator.isDone():
+            file_api_mfn  = OpenMaya.MFnDependencyNode(iterator.currentItem())
+            file_api_plug = file_api_mfn.findPlug('ftn')
+            data.setdefault('{0}.{1}'.format(shader, attr), file_api_plug.asString())
+            iterator.next()
+
+    return data
+
+
+
+def get_all_tex_data():
+    '''
+    '''
+    data = dict()
+    for shd in mc.ls(typ='aiStandardSurface'):
+        data[shd] = get_ai_tex_data(shd)
+    return data
+
+
+
+if __name__ == '__main__':
+    print(get_all_tex_data())
